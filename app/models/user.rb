6d125547953163bc3cb16ee935a5836b5b70b693
Brennan Willingham <brennanw22@gmail.com>
2015-06-14 23:29:14 -0500

    Added validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, :first_name, :last_name, presence: true
+  validates :first_name, :last_name, length: { minimum: 3 }
+  validates :email, format: { with: /.+@.+\..+/, message: "must be an email address" }, uniqueness: true
+  validates :password, confirmation: true
+  validates :password, :password_confirmation, presence: { on: :create }
 end
