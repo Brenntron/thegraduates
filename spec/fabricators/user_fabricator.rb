26acb1cb268451ed18a24dcd15f3e0a049b90ee4
Brennan Willingham <brennanw22@gmail.com>
2015-06-15 12:22:08 -0500

    Model tests pass

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,8 +1,13 @@
 Fabricator(:user) do
-  first_name       { Faker::Name.first_name }
-  last_name        { Faker::Name.last_name }
-  email            { Faker::Internet.email }
-  linkedin         { Faker::Internet.user_name }
-  twitter          { Faker::Internet.user_name }
-  current_employer { Faker::Company.name }
+  password = Faker::Internet.password(8)
+
+  first_name            { Faker::Name.first_name }
+  last_name             { Faker::Name.last_name }
+  email                 { Faker::Internet.email }
+  linkedin              { Faker::Internet.user_name }
+  twitter               { Faker::Internet.user_name }
+  current_employer      { Faker::Company.name }
+  bio                   { Faker::Lorem.paragraph(2) }
+  password              { password }
+  password_confirmation { password }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of(:email) }
-    it { should validate_uniquesness_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:first_name) }
     it { should validate_length_of(:first_name).is_at_least(3) }
     it { should validate_presence_of(:last_name) }
@@ -15,16 +15,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_confirmation).on(:create) }
     it { should_not validate_presence_of(:password_confirmation).on(:update) }
     it "should allow valid values for email" do
-      should all_value("brennan@brennanwilligham.com", "a@b.co.uk", "brennan+hash@example.com").for(:email)
+      should allow_value("brennan@brennanwilligham.com", "a@b.co.uk", "brennan+hash@example.com").for(:email)
     end
     describe "should not save if email is !unique" do
-      it "should have an error message 'email already in use'" do
+      it "should have an error message 'has already been taken'" do
         Fabricate(:user, email: "brennan@willingham.com")
         user = Fabricate.build(:user, email: "brennan@willingham.com")
         user.valid?.should be_falsey
         (user.errors[:email].any?).should be_truthy
         error = user.errors[:email][0]
-        expect(error).to eq('email already in use')
+        expect(error).to eq('has already been taken')
       end
     end
     describe "should be invalid if email is not formatted correctly" do
