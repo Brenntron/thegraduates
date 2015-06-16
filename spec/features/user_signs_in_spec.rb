697aad952dbac1830d39bfb3e9bb59eb459200ad
Brennan Willingham <brennanw22@gmail.com>
2015-06-16 15:01:15 -0500

    Finished sign in tests and fixed sign up tests.

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,1 +1,50 @@
+feature "user signs in" do
 
+  before do
+    visit "/"
+    click_on "Sign In"
+    page.should_not have_link("Sign In")
+  end
+
+  scenario "Returning user signs in" do
+    user = Fabricate(:user, name: "Jan")
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password1"
+    click_button "Sign In"
+    page.shoudl have_content("Welcome back, Jan")
+    page.should_not have_content("Sign In")
+    page.should_not have_content("Sign Up")
+    page.should have_content("Sign Out")
+    click_on "Sign Out"
+    page.should have_content("You have been signed out")
+    page.should have_content("Sign In")
+    page.should_not have_content("Sign Out")
+  end
+
+  scenario "returning user attempts to sign in with incorrect password" do
+    user = Fabricate(:user, name: "Bob")
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "wrongpassword"
+    click_button "Sign In"
+    page.should have_content("We could not sign you in. Please check your email/password and try again.")
+    page.should_not have_content("Create your account")
+    page.should_not have_content("Password confirmation")
+    field_labeled("Email").value.should == user.email
+    fill_in "Password", with: "password1"
+    click_button "Sign In"
+    page.should have_content("Welcome back, Bob!")
+  end
+
+  scenario "user signs in with wrong email" do
+    Fabricate(:user, email: "jimbo@example.com", password: "Awesomeness", password_confirmation: "Awesomeness")
+    fill_in "Email", with: "bobbie@example.com"
+    fill_in "Password", with: "Awesomeness"
+    click_on "Sign In"
+    page.should have_content("We could not sign you in. Please check your email/password and try again.")
+  end
+
+  scenario "user signs in with blanks" do
+    click_on "Sign In"
+    page.should have_content("We could not sign up in. Please check you email/password and try again.")
+  end
+end
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -3,40 +3,39 @@ feature "User Signs Up" do
   background do
     visit root_path
     click_on "Sign Up"
-    joe = Fabricate(:user)
   end
 
   scenario "Happy sign in path" do
     page.should_not have_link("Sign Up")
-    fill_in "First Name", with: "#{joe.first_name}"
-    fill_in "Last Name", with: "#{joe.last_name}"
-    fill_in "Email", with: "#{joe.email}"
-    fill_in "Password", with: "#{joe.password}"
-    fill_in "Password Confirmation", with: "#{joe.password_confirmation}"
-    fill_in "Linkedin", with: "#{joe.linkedin}"
-    fill_in "Twitter", with: "#{joe.twitter}"
-    fill_in "Bio", with: "#{joe.bio}"
-    fill_in "Current Employer", with: "#{joe.current_employer}"
+    fill_in "First Name", with: "Edward"
+    fill_in "Last Name", with: "Norton"
+    fill_in "Email", with: "edward@norton.com"
+    fill_in "Password", with: "fightclub"
+    fill_in "Password Confirmation", with: "fightclub"
+    fill_in "Linkedin", with: "www.linkedin.com/edwardnorton"
+    fill_in "Twitter", with: "www.twitter.com/edwardnorton"
+    fill_in "Bio", with: Faker::Lorem.paragraph(2)
+    fill_in "Current Employer", with: Faker::Company.name
     click_button "Sign Up"
-    page.should have_content("Welcome #{joe.first_name}!")
+    page.should have_content("Welcome Edward!")
     click_on "Sign out"
     click_on "Sign in"
-    fill_in "Email", with: "#{joe.email}"
-    fill_in "Password", with: "#{joe.password}"
+    fill_in "Email", with: "edward@norton.com"
+    fill_in "Password", with: "fightclub"
     click_button "Sign In"
-    page.should have_content("Welcome back, #{joe.first_name}!")
+    page.should have_content("Welcome back, Edward!")
   end
 
   scenario "Error Path" do
     fill_in "First Name", with: ""
     fill_in "Last Name", with: ""
-    fill_in "Email", with: "#{joe.email}"
-    fill_in "Password", with: "#{joe.password}"
+    fill_in "Email", with: "edward@norton.com"
+    fill_in "Password", with: "fightclub"
     fill_in "Password Confirmation", with: "food"
-    fill_in "Linkedin", with: "#{joe.linkedin}"
-    fill_in "Twitter", with: "#{joe.twitter}"
-    fill_in "Bio", with: "#{joe.bio}"
-    fill_in "Current Employer", with: "#{joe.current_employer}"
+    fill_in "Linkedin", with: "www.linkedin.com/edwardnorton"
+    fill_in "Twitter", with: "www.twitter.com/edwardnorton"
+    fill_in "Bio", with: Faker::Lorem.paragraph(2)
+    fill_in "Current Employer", with: Faker::Company.name
     click_button "Sign Up"
     page.should have_alert("Please fix the errors to continue.")
 
@@ -45,42 +44,42 @@ feature "User Signs Up" do
     page.should have_error("must be an email address", on: "Email")
     page.should have_error("doesn't match Password", on: "Password confirmation")
 
-    fill_in "First Name", with: "#{joe.first_name}"
-    fill_in "Last Name", with: "#{joe.last_name}"
-    fill_in "Email", with: "#{joe.email}"
-    fill_in "Password", with: "#{joe.password}"
-    fill_in "Password Confirmation", with: "#{joe.password_confirmation}"
-    fill_in "Linkedin", with: "#{joe.linkedin}"
-    fill_in "Twitter", with: "#{joe.twitter}"
-    fill_in "Bio", with: "#{joe.bio}"
-    fill_in "Current Employer", with: "#{joe.current_employer}"
+    fill_in "First Name", with: "Edward"
+    fill_in "Last Name", with: "Nrton"
+    fill_in "Email", with: "edward@norton.com"
+    fill_in "Password", with: "fightclub"
+    fill_in "Password Confirmation", with: "fightclub"
+    fill_in "Linkedin", with: "www.linkedin.com/edwardnorton"
+    fill_in "Twitter", with: "www.twitter.com/edwardnorton"
+    fill_in "Bio", with: Faker::Lorem.paragraph(2)
+    fill_in "Current Employer", with: Faker::Company.name
     click_on "Sign Up"
-    page.should have_content("Welcome, #{joe.first_name}!")
+    page.should have_content("Welcome, Edward!")
   end
 
   scenario "invalid urls" do
     page.should_not have_link("Sign Up")
-    fill_in "First Name", with: "#{joe.first_name}"
-    fill_in "Last Name", with: "#{joe.last_name}"
-    fill_in "Email", with: "#{joe.email}"
-    fill_in "Password", with: "#{joe.password}"
-    fill_in "Password Confirmation", with: "#{joe.password_confirmation}"
+    fill_in "First Name", with: "Edward"
+    fill_in "Last Name", with: "Norton"
+    fill_in "Email", with: "edward@norton.com"
+    fill_in "Password", with: "fightclub"
+    fill_in "Password Confirmation", with: "fightclub"
     fill_in "Linkedin", with: "gobulldyguck"
     fill_in "Twitter", with: "notaurl"
-    fill_in "Bio", with: "#{joe.bio}"
-    fill_in "Current Employer", with: "#{joe.current_employer}"
+    fill_in "Bio", with: Faker::Lorem.name
+    fill_in "Current Employer", with: Faker::Company.name
     click_button "Sign Up"
     page.should have_alert("Please fix the errors to continue.")
-    fill_in "First Name", with: "#{joe.first_name}"
-    fill_in "Last Name", with: "#{joe.last_name}"
-    fill_in "Email", with: "#{joe.email}"
-    fill_in "Password", with: "#{joe.password}"
-    fill_in "Password Confirmation", with: "#{joe.password_confirmation}"
-    fill_in "Linkedin", with: "#{joe.linkedin}"
-    fill_in "Twitter", with: "#{joe.twitter}"
-    fill_in "Bio", with: "#{joe.bio}"
-    fill_in "Current Employer", with: "#{joe.current_employer}"
+    fill_in "First Name", with: "Edward"
+    fill_in "Last Name", with: "Norton"
+    fill_in "Email", with: "edward@norton.com"
+    fill_in "Password", with: "fightclub"
+    fill_in "Password Confirmation", with: "fightclub"
+    fill_in "Linkedin", with: "www.linkedin.com/edwardnorton"
+    fill_in "Twitter", with: "www.twitter.com/edwardnorton"
+    fill_in "Bio", with: Faker::Lorem.paragraph(2)
+    fill_in "Current Employer", with: Faker::Company.name
     click_button "Sign Up"
-    page.should have_content("Welcome #{joe.first_name}!")
+    page.should have_content("Welcome Edward!")
   end
 end
